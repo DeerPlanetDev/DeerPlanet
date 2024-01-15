971828e478a235095b7b32b5ec19c18d2a2d04ad
Santiago Ávila <a01663663@tec.mx>
2024-01-15 16:28:00 -0600

    added main menu scripts

---
+++ Assets/Scripts/MainMenu/GameSettings.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    public static float musicVolume = 1f;
+    public static float sfxVolume = 1f;
+
+}
